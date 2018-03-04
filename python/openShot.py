c1e1edff9e79975ee917955ee4e1f0dc66d0749c
Efrizal Hardiman <efrizal.hardiman@gmail.com>
2018-03-04 18:40:01 +0900

    Initial commit

---
+++ python/openShot.py
@@ -0,0 +1,22 @@
+from PySide.QtGui import *
+from PySide.QtCore import *
+import sys
+
+class Panel(QWidget):
+    def __init__(self):
+        super(Panel, self).__init__()
+
+        label = QLabel()
+        label.setText("Episode")
+
+        comboBox = QComboBox()
+        comboBox.addItem("")
+
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(label)
+        hLayout.addWidget(comboBox)
+        
+        self.setLayout(hLayout)
+
+widget = Panel()
+widget.show()
